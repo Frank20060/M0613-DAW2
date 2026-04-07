032d42a59ec402ccaf344ac1ba0f99073a8ddf8f
Frank20060 <villarredondofrank@fpllefia.com>
2026-04-07 20:12:37 +0200

    next proyecto basico

---
+++ src/app/(site)/blog/data.js
@@ -0,0 +1,14 @@
+export const posts = [
+  {
+    slug: "el-meu-primer-post",
+    title: "El meu primer post",
+    excerpt: "Introducció al blog.",
+    content: "Aquest és el contingut complet del primer article.",
+  },
+  {
+    slug: "nextjs-app-router",
+    title: "Next.js App Router",
+    excerpt: "Rutes i layouts.",
+    content: "Aquí expliques com funciona l'App Router.",
+  },
+];
